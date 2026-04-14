eed3531e1c1c8a4989460960d52277511fc52569
Revant <revant0501@gmail.com>
2026-04-15 02:48:34 +0530

    More Changes in Settings page UI

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
-class Settings extends StatelessWidget {
+class Settings extends StatefulWidget {
   const Settings({super.key});
 
   @override
+  State<Settings> createState()=> _SettingsState();
+}
+class _SettingsState extends State<Settings>{
+  TimeOfDay startTime = TimeOfDay(hour: 22, minute: 0);
+  TimeOfDay endTime=TimeOfDay(hour: 7, minute: 0);
+  bool isEnabled=true;
+  bool enabled=true;
+  String selectSound = "Default";
+  bool quietEnabled=false;
+  @override
   Widget build(BuildContext context) {
-
-    bool isEnabled=true;
-    bool enabled=true;
-    String selectSound = "Default";
     return Scaffold(
       appBar: AppBar(
         title: const Text("S E T T I N G S"),
@@ -54,15 +60,84 @@ class Settings extends StatelessWidget {
                   ],
               ),
             ),
+            const Text("Quiet Hours",style:TextStyle(fontSize:22)),
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12)
+              ),
+              elevation: 2,
+              margin: EdgeInsets.symmetric(horizontal: 16,vertical: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SwitchListTile(
+                      title:Text("Enable Quiet Mode"),
+                      subtitle: Text("QuietEnabledts during selected Time"),
+                      value: quietEnabled,
+                      onChanged: (val)=>{}
+                  ),
+                  Divider(),
+                  ListTile(
+                    title: Text("Start Time"),
+                    subtitle: Text("Select quiet hours time start"),
+                    trailing: Text(
+                      startTime.format(context),style: TextStyle(fontSize: 20),
+                    ),
+                    onTap: pickStartTime,
+                  ),
+                  Divider(),
+                  ListTile(
+                    title: Text("End Time"),
+                    subtitle: Text("Select quiet hours time end"),
+                    trailing: Text(
+                      endTime.format(context),style: TextStyle(fontSize: 20),
+                    ),
+                    onTap: pickEndTime,
+                  )
+                ],
+              ),
+            ),
+            const Text("Appearance",style:TextStyle(fontSize: 22)),
+            Card(
+              shape:RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              elevation: 2,
+              margin: EdgeInsets.symmetric(horizontal: 16,vertical: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ListTile(
+                      title:Text("Theme",style: TextStyle(fontSize: 20),)
+                  ) ,
+                ],
+              ),
+            ),
           ],
         ),
       ),
     );
   }
-
+  Future<void> pickStartTime()async{
+    TimeOfDay? picked=await showTimePicker(context: context, initialTime: startTime,);
+    if(picked!=null){
+      setState((){
+        startTime=picked;
+      });
+    }
+  }
+  Future<void> pickEndTime()async {
+    TimeOfDay? picked=await showTimePicker(context: context, initialTime: endTime,);
+    if(picked!=null){
+      setState(() {
+        endTime=picked;
+      });
+    }
+  }
   void setAlert(bool Function() param0) {}
 
   void setVibrate(bool Function() param0){}
 
   void setSound(String Function() param0) {}
+
 }
