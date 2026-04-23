10f1e8727a8ea1b4b1cbb5ff7914c91397babab9
Revant <revant0501@gmail.com>
2026-04-23 23:58:18 +0530

    Changes  in UI of  about page

--- lib/pages/about_app.dart
+++ lib/pages/about_app.dart
@@ -24,8 +24,55 @@ class AboutApp extends StatelessWidget{
             "Version 1.0.0",
             style: TextStyle(color: Colors.grey),
           ),
+          Card(
+            margin: EdgeInsets.all(12),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(12),
+              child: Text(
+                "CodeAlert helps developers track important alerts and updates in real-time.",
+              ),
+            ),
+          ),
+          Card(
+            margin: EdgeInsets.all(12),
+            child: ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Developers"),
+              subtitle: Text("Revant Singh\nNidhiansh Chauhan" ,style: TextStyle(fontSize: 16),),
+            ),
+          ),
+          Card(
+            margin: EdgeInsets.all(12),
+            child: Column(
+              children: [
+                ListTile(
+                  leading: Icon(Icons.code),
+                  title: Text("GitHub"),
+                  trailing: Icon(Icons.open_in_new),
+                  onTap: () {},
+                ),
+                Divider(),
+                ListTile(
+                  leading: Icon(Icons.star),
+                  title: Text("Rate App"),
+                  trailing: Icon(Icons.arrow_forward_ios, size: 16),
+                  onTap: () {},
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
   }
+  // Future<void> openGitHub() async {
+  //   final Uri url = Uri.parse("https://github.com/REVANTSINGH01/Code-Alert");
+  //
+  //   if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
+  //     throw 'Could not launch $url';
+  //   }
+  // }
 }
