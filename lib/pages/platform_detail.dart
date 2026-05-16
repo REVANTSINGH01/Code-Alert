e7ff060d19cb627375b174a6c0874f572ae9699d
Revant <revant0501@gmail.com>
2026-05-16 23:58:47 +0530

    Created user platform details page

---
+++ lib/pages/platform_detail.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PlatformDetail extends StatefulWidget{
+  const PlatformDetail({super.key});
+
+  @override
+  State<PlatformDetail> createState() => _PlatformDetail();
+}
+class _PlatformDetail extends State<PlatformDetail>{
+
+}
