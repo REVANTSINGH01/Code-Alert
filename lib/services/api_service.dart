fe6455ac3bac790574b1d5e10e68f9c2b313e6e5
Revant <revant0501@gmail.com>
2026-06-21 23:47:31 +0530

    Fixed reminders bug occuring after login

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -193,14 +193,22 @@
     // =========================
   
     static Future<List<dynamic>> getReminders() async {
+      final prefs = await SharedPreferences.getInstance();
+      final token = prefs.getString("token");
+
       final response = await http.get(
         Uri.parse("$baseUrl/reminders/"),
+
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer $token",
+        },
       );
-  
+
       if (response.statusCode == 200) {
         return jsonDecode(response.body);
       } else {
-        throw Exception("Failed to load reminders");
+        throw Exception("Failed to load reminders: ${response.body}");
       }
     }
   
